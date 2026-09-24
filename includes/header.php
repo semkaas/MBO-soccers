96b109602632aef531b55d719a1762e0f5b5a642
semkaas <6052414@mborijnland.nl>
2026-09-24 13:55:29 +0200

    ??

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,15 @@
 <section class = "header">
-    <p> header </p>
-</section>
+    <article class = "hamburger" onclick="toggleMenu()">
+            <h1>aaaayy</h1>
+        </article>
+    </section>  
+<nav id="dropdown-menu" class="menu-hidden">
+        <ul class="hamburgermenu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="lunchdiner.php">lunch en dinner</a></li>
+            <li><a href="navigatie.php">locatie</a></li>
+            <li><a href="reseveren.php">reseveren</a></li>
+            <li><a href="vacatures.php">bij ons werken</a></li>
+        </ul>
+    
+</nav>
